af73e14ee0c044dd969c145272b9961b54b14f08
sehonge <sehong1602@naver.com>
2022-04-18 22:17:34 +0900

    first commit - mongoDB 설정

---
+++ src/main/kotlin/com/example/be/controller/BoardController.kt
@@ -0,0 +1,13 @@
+package com.example.be.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/board")
+class BoardController {
+
+    fun getBoard(userId: Long): String {
+        return "ok";
+    }
+}
---
+++ src/test/kotlin/com/example/be/BeApplication.kt
@@ -0,0 +1,13 @@
+package com.example.be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+class BeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BeApplication>(*args)
+
+}
