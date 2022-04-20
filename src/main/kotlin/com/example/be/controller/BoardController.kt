f68b36d62bf11463a5e9e8a24189fc9ac752e672
sehonge <sehong1602@naver.com>
2022-04-20 16:40:01 +0900

    BoardController 뼈대 작성

--- src/main/kotlin/com/example/be/controller/BoardController.kt
+++ src/main/kotlin/com/example/be/controller/BoardController.kt
@@ -1,13 +1,42 @@
 package com.example.be.controller
 
+import com.example.be.service.BoardService
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/board")
-class BoardController {
+@RequiredArgsConstructor
+class BoardController(val boardService: BoardService) {
 
+    @GetMapping
     fun getBoard(userId: Long): String {
         return "ok!";
     }
+
+    @GetMapping("/count")
+    fun getBoardCOunt() {
+        return TODO();
+    }
+
+    @PostMapping
+    fun insertBoard() {
+        return TODO();
+    }
+
+    @PutMapping
+    fun updateBoard() {
+        return TODO();
+    }
+
+    @DeleteMapping
+    fun deleteBoard() {
+        return TODO();
+    }
+
 }
