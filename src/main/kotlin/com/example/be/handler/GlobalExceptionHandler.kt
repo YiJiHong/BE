a102255fc02dadc5621fc51ae894bc50326294f7
sehonge <sehong1602@naver.com>
2022-05-23 16:31:35 +0900

    NotValidUserRegisterFormException 추가

--- src/main/kotlin/com/example/be/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/be/handler/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.example.be.handler
 import com.example.be.dto.ErrorResponse
 import com.example.be.exception.NoneBoardException
 import com.example.be.exception.NoneUserException
+import com.example.be.exception.NotValidUserRegisterFormException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -27,4 +28,8 @@ class GlobalExceptionHandler {
         return ResponseEntity(ErrorResponse(msg = e.msg), HttpStatus.NOT_FOUND)
     }
 
+    @ExceptionHandler(NotValidUserRegisterFormException::class)
+    fun handleNotValidUserRegisterFormException(e: NotValidUserRegisterFormException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse(msg = e.msg), HttpStatus.BAD_REQUEST)
+    }
 }
